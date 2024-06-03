f4ba32a88d3f931392a419af24ce44defcc82c86
Isaac Wasserman <isaacrwasserman@gmail.com>
2024-06-03 14:23:46 -0400

    runs now

---
+++ utils.py
@@ -0,0 +1,27 @@
+import torch
+
+
+def same_padding(kernel_size):
+    if kernel_size % 2 == 1:
+        top = left = right = bottom = (kernel_size - 1) // 2
+    else:
+        top = left = (kernel_size - 1) // 2
+        bottom = right = (kernel_size - 1) // 2 + 1
+    return left, right, top, bottom
+
+
+def img_to_patch(x, patch_size, flatten_channels=True):
+    """
+    Args:
+        x: Tensor representing the image of shape [B, C, H, W]
+        patch_size: Number of pixels per dimension of the patches (integer)
+        flatten_channels: If True, the patches will be returned in a flattened format
+                           as a feature vector instead of a image grid.
+    """
+    B, C, H, W = x.shape
+    x = x.reshape(B, C, H // patch_size, patch_size, W // patch_size, patch_size)
+    x = x.permute(0, 2, 4, 1, 3, 5)  # [B, H', W', C, p_H, p_W]
+    x = x.flatten(1, 2)  # [B, H'*W', C, p_H, p_W]
+    if flatten_channels:
+        x = x.flatten(2, 4)  # [B, H'*W', C*p_H*p_W]
+    return x
