3360585241323ad56ec8023470eb11ac585c353e
Isaac Wasserman <isaacrwasserman@gmail.com>
2024-07-23 22:02:32 +0000

    updated setup procedure

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 pip install -r requirements.txt
-mkdir runs
 curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-linux-x86_64.tar.gz
 tar -xf google-cloud-cli-linux-x86_64.tar.gz
 ./google-cloud-sdk/install.sh
