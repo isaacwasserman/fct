025634c138e2658b52174899a4bb541919a2d0ff
Isaac Wasserman <isaacrwasserman@gmail.com>
2024-07-23 14:50:24 -0400

    made segformer decoder fc

--- fc_segformer.py
+++ fc_segformer.py
@@ -177,11 +177,81 @@ class FC_SegformerModel(SegformerModel):
         self.post_init()
 
 
+class FC_SegformerMLP(nn.Module):
+    def __init__(self, config: SegformerConfig, input_dim):
+        super().__init__()
+        self.proj = nn.Conv2d(input_dim, config.decoder_hidden_size, kernel_size=1)
+
+    def forward(self, hidden_states: torch.Tensor):
+        hidden_states = self.proj(hidden_states)
+        hidden_states = hidden_states.permute(0, 2, 3, 1).contiguous()
+        hidden_states = hidden_states.view(hidden_states.size(0), -1, hidden_states.size(-1))
+        return hidden_states
+
+
+class FC_SegformerDecodeHead(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        # linear layers which will unify the channel dimension of each of the encoder blocks to the same config.decoder_hidden_size
+        mlps = []
+        for i in range(config.num_encoder_blocks):
+            mlp = FC_SegformerMLP(config, input_dim=config.hidden_sizes[i])
+            mlps.append(mlp)
+        self.linear_c = nn.ModuleList(mlps)
+
+        # the following 3 layers implement the ConvModule of the original implementation
+        self.linear_fuse = nn.Conv2d(
+            in_channels=config.decoder_hidden_size * config.num_encoder_blocks,
+            out_channels=config.decoder_hidden_size,
+            kernel_size=1,
+            bias=False,
+        )
+        self.batch_norm = nn.BatchNorm2d(config.decoder_hidden_size)
+        self.activation = nn.ReLU()
+
+        self.dropout = nn.Dropout(config.classifier_dropout_prob)
+        self.classifier = nn.Conv2d(config.decoder_hidden_size, config.num_labels, kernel_size=1)
+
+        self.config = config
+
+    def forward(self, encoder_hidden_states: torch.FloatTensor) -> torch.Tensor:
+        batch_size = encoder_hidden_states[-1].shape[0]
+
+        all_hidden_states = ()
+        for encoder_hidden_state, mlp in zip(encoder_hidden_states, self.linear_c):
+            if self.config.reshape_last_stage is False and encoder_hidden_state.ndim == 3:
+                height = width = int(math.sqrt(encoder_hidden_state.shape[-1]))
+                encoder_hidden_state = (
+                    encoder_hidden_state.reshape(batch_size, height, width, -1).permute(0, 3, 1, 2).contiguous()
+                )
+
+            # unify channel dimension
+            height, width = encoder_hidden_state.shape[2], encoder_hidden_state.shape[3]
+            encoder_hidden_state = mlp(encoder_hidden_state)
+            encoder_hidden_state = encoder_hidden_state.permute(0, 2, 1)
+            encoder_hidden_state = encoder_hidden_state.reshape(batch_size, -1, height, width)
+            # upsample
+            encoder_hidden_state = nn.functional.interpolate(
+                encoder_hidden_state, size=encoder_hidden_states[0].size()[2:], mode="bilinear", align_corners=False
+            )
+            all_hidden_states += (encoder_hidden_state,)
+
+        hidden_states = self.linear_fuse(torch.cat(all_hidden_states[::-1], dim=1))
+        hidden_states = self.batch_norm(hidden_states)
+        hidden_states = self.activation(hidden_states)
+        hidden_states = self.dropout(hidden_states)
+
+        # logits are of shape (batch_size, num_labels, height/4, width/4)
+        logits = self.classifier(hidden_states)
+
+        return logits
+
+
 class FC_SegformerForSemanticSegmentation(SegformerForSemanticSegmentation):
     def __init__(self, config):
         super().__init__(config)
         self.segformer = FC_SegformerModel(config)
-        self.decode_head = SegformerDecodeHead(config)
+        self.decode_head = FC_SegformerDecodeHead(config)
 
         # Initialize weights and apply final processing
         self.post_init()
