7e0a562aec98c9ff38abd108b97e7a9cf85aa252
Your Name <you@example.com>
2024-07-24 19:17:54 +0000

    made unet smaller

--- unet/unet.py
+++ unet/unet.py
@@ -2,8 +2,6 @@ import os
 import torch
 import torchvision.transforms.v2 as transforms
 from fct import FC_Attention
-
-# from pascal_utils import PascalTrainer
 from utils import *
 
 
@@ -17,6 +15,7 @@ class UNetConfig:
         feed_forward_kernel_size=3,
         num_labels=21,
         input_resolution=(256, 256),
+        max_channels=512,
     ):
         self.hidden_size = hidden_size
         self.num_attention_heads = num_attention_heads
@@ -25,6 +24,7 @@ class UNetConfig:
         self.feed_forward_kernel_size = feed_forward_kernel_size
         self.num_labels = num_labels
         self.input_resolution = input_resolution
+        self.max_channels = max_channels
 
     def __call__(self, **kwargs):
         new_config = self.__dict__.copy()
@@ -132,105 +132,122 @@ class UNet(torch.nn.Module):
         super().__init__()
         self.config = config
         self.input_resolution = config.input_resolution
+        self.max_channels = config.max_channels
         self.resolutions = [tuple(map(lambda x: x // (2**i), self.input_resolution)) for i in range(5)]
+        self.depths = [self.max_channels // (2**i) for i in range(4, -1, -1)]
         self.encoder_0 = UNetTransformerEncoderBlock(
-            config, input_resolution=self.resolutions[0], input_channels=3, output_channels=64
+            config, input_resolution=self.resolutions[0], input_channels=3, output_channels=self.depths[0]
         )
         self.encoder_1 = UNetTransformerEncoderBlock(
-            config, input_resolution=self.resolutions[1], input_channels=64, output_channels=128
+            config, input_resolution=self.resolutions[1], input_channels=self.depths[0], output_channels=self.depths[1]
         )
         self.encoder_2 = UNetTransformerEncoderBlock(
-            config, input_resolution=self.resolutions[2], input_channels=128, output_channels=256
+            config, input_resolution=self.resolutions[2], input_channels=self.depths[1], output_channels=self.depths[2]
         )
         self.encoder_3 = UNetTransformerEncoderBlock(
-            config, input_resolution=self.resolutions[3], input_channels=256, output_channels=512
+            config, input_resolution=self.resolutions[3], input_channels=self.depths[2], output_channels=self.depths[3]
         )
         self.encoder_4 = UNetTransformerEncoderBlock(
-            config, input_resolution=self.resolutions[4], input_channels=512, output_channels=1024
+            config, input_resolution=self.resolutions[4], input_channels=self.depths[3], output_channels=self.depths[4]
         )
 
-        self.upsampler_0 = torch.nn.ConvTranspose2d(1024, 512, kernel_size=2, stride=2)
+        self.upsampler_0 = torch.nn.ConvTranspose2d(self.depths[4], self.depths[3], kernel_size=2, stride=2)
         self.decoder_0 = UNetTransformerDecoderBlock(
-            config, input_resolution=self.resolutions[3], input_channels=1024, output_channels=512, block_index="decoder_0"
+            config,
+            input_resolution=self.resolutions[3],
+            input_channels=self.depths[4],
+            output_channels=self.depths[3],
+            block_index="decoder_0",
         )
-        self.upsampler_1 = torch.nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
+        self.upsampler_1 = torch.nn.ConvTranspose2d(self.depths[3], self.depths[2], kernel_size=2, stride=2)
         self.decoder_1 = UNetTransformerDecoderBlock(
-            config, input_resolution=self.resolutions[2], input_channels=512, output_channels=256, block_index="decoder_1"
+            config,
+            input_resolution=self.resolutions[2],
+            input_channels=self.depths[3],
+            output_channels=self.depths[2],
+            block_index="decoder_1",
         )
-        self.upsampler_2 = torch.nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
+        self.upsampler_2 = torch.nn.ConvTranspose2d(self.depths[2], self.depths[1], kernel_size=2, stride=2)
         self.decoder_2 = UNetTransformerDecoderBlock(
-            config, input_resolution=self.resolutions[1], input_channels=256, output_channels=128, block_index="decoder_2"
+            config,
+            input_resolution=self.resolutions[1],
+            input_channels=self.depths[2],
+            output_channels=self.depths[1],
+            block_index="decoder_2",
         )
-        self.upsampler_3 = torch.nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
+        self.upsampler_3 = torch.nn.ConvTranspose2d(self.depths[1], self.depths[0], kernel_size=2, stride=2)
         self.decoder_3 = UNetTransformerDecoderBlock(
-            config, input_resolution=self.resolutions[0], input_channels=128, output_channels=64, block_index="decoder_3"
+            config,
+            input_resolution=self.resolutions[0],
+            input_channels=self.depths[1],
+            output_channels=self.depths[0],
+            block_index="decoder_3",
         )
-
-        self.segmentation_head = torch.nn.Conv2d(64, config.num_labels, kernel_size=1)
 
     def forward(self, x):
         B = x.shape[0]
 
         encoder_0_out = self.encoder_0(x)
-        assert_shape(encoder_0_out, (B, 64, *self.resolutions[0]))
+        assert_shape(encoder_0_out, (B, self.depths[0], *self.resolutions[0]))
         encoder_0_pooled = torch.nn.functional.max_pool2d(encoder_0_out, kernel_size=2, stride=2)
-        assert_shape(encoder_0_pooled, (B, 64, *self.resolutions[1]))
+        assert_shape(encoder_0_pooled, (B, self.depths[0], *self.resolutions[1]))
 
         encoder_1_out = self.encoder_1(encoder_0_pooled)
-        assert_shape(encoder_1_out, (B, 128, *self.resolutions[1]))
+        assert_shape(encoder_1_out, (B, self.depths[1], *self.resolutions[1]))
         encoder_1_pooled = torch.nn.functional.max_pool2d(encoder_1_out, kernel_size=2, stride=2)
-        assert_shape(encoder_1_pooled, (B, 128, *self.resolutions[2]))
+        assert_shape(encoder_1_pooled, (B, self.depths[1], *self.resolutions[2]))
 
         encoder_2_out = self.encoder_2(encoder_1_pooled)
-        assert_shape(encoder_2_out, (B, 256, *self.resolutions[2]))
+        assert_shape(encoder_2_out, (B, self.depths[2], *self.resolutions[2]))
         encoder_2_pooled = torch.nn.functional.max_pool2d(encoder_2_out, kernel_size=2, stride=2)
-        assert_shape(encoder_2_pooled, (B, 256, *self.resolutions[3]))
+        assert_shape(encoder_2_pooled, (B, self.depths[2], *self.resolutions[3]))
 
         encoder_3_out = self.encoder_3(encoder_2_pooled)
-        assert_shape(encoder_3_out, (B, 512, *self.resolutions[3]))
+        assert_shape(encoder_3_out, (B, self.depths[3], *self.resolutions[3]))
         encoder_3_pooled = torch.nn.functional.max_pool2d(encoder_3_out, kernel_size=2, stride=2)
-        assert_shape(encoder_3_pooled, (B, 512, *self.resolutions[4]))
+        assert_shape(encoder_3_pooled, (B, self.depths[3], *self.resolutions[4]))
 
         encoder_4_out = self.encoder_4(encoder_3_pooled)
-        assert_shape(encoder_4_out, (B, 1024, *self.resolutions[4]))
+        assert_shape(encoder_4_out, (B, self.depths[4], *self.resolutions[4]))
         encoder_4_upsampled = self.upsampler_0(encoder_4_out)
-        assert_shape(encoder_4_upsampled, (B, 512, *self.resolutions[3]))
+        assert_shape(encoder_4_upsampled, (B, self.depths[3], *self.resolutions[3]))
 
         decoder_0_in = torch.concat([encoder_4_upsampled, encoder_3_out], dim=1)
         decoder_0_out = self.decoder_0(decoder_0_in)
-        assert_shape(decoder_0_out, (B, 512, *self.resolutions[3]))
+        assert_shape(decoder_0_out, (B, self.depths[3], *self.resolutions[3]))
         decoder_0_upsampled = self.upsampler_1(decoder_0_out)
-        assert_shape(decoder_0_upsampled, (B, 256, *self.resolutions[2]))
+        assert_shape(decoder_0_upsampled, (B, self.depths[2], *self.resolutions[2]))
 
         decoder_1_in = torch.concat([decoder_0_upsampled, encoder_2_out], dim=1)
         decoder_1_out = self.decoder_1(decoder_1_in)
-        assert_shape(decoder_1_out, (B, 256, *self.resolutions[2]))
+        assert_shape(decoder_1_out, (B, self.depths[2], *self.resolutions[2]))
         decoder_1_upsampled = self.upsampler_2(decoder_1_out)
-        assert_shape(decoder_1_upsampled, (B, 128, *self.resolutions[1]))
+        assert_shape(decoder_1_upsampled, (B, self.depths[1], *self.resolutions[1]))
 
         decoder_2_in = torch.concat([decoder_1_upsampled, encoder_1_out], dim=1)
         decoder_2_out = self.decoder_2(decoder_2_in)
-        assert_shape(decoder_2_out, (B, 128, *self.resolutions[1]))
+        assert_shape(decoder_2_out, (B, self.depths[1], *self.resolutions[1]))
         decoder_2_upsampled = self.upsampler_3(decoder_2_out)
-        assert_shape(decoder_2_upsampled, (B, 64, *self.resolutions[0]))
+        assert_shape(decoder_2_upsampled, (B, self.depths[0], *self.resolutions[0]))
 
         decoder_3_in = torch.concat([decoder_2_upsampled, encoder_0_out], dim=1)
         decoder_3_out = self.decoder_3(decoder_3_in)
-        assert_shape(decoder_3_out, (B, 64, *self.resolutions[0]))
+        assert_shape(decoder_3_out, (B, self.depths[0], *self.resolutions[0]))
 
-        segmentation = self.segmentation_head(decoder_3_out)
-        assert_shape(segmentation, (B, self.config.num_labels, *self.resolutions[0]))
-
-        return segmentation
+        return decoder_3_out
 
 
 if __name__ == "__main__":
+    import time
+
+    instantiation_start = time.time()
     unet = UNet(UNetConfig()).to("cuda")
-    # print number of parameters
-    for name, param in unet.named_parameters():
-        print(f"Layer: {name} | Size: {param.size()}")
+    dummy_input = torch.randn(4, 3, 256, 256).to("cuda")
+    print(f"Instantiation time: {time.time() - instantiation_start:.2f}s")
     print(f"Number of parameters: {sum(p.numel() for p in unet.parameters() if p.requires_grad)}")
-    dummy_input = torch.randn(1, 3, 256, 256).to("cuda")
-    dummy_output = unet(dummy_input)
+
+    processing_start = time.time()
+    for _ in range(100):
+        dummy_output = unet(dummy_input)
     assert_shape(dummy_output, (1, 21, 256, 256))
-    print("UNet test passed!")
+    print(f"Processing time: {time.time() - processing_start:.2f}s")
